0fca27323fef7c2968e52fe4408005f7a2ba4312
Real-Luxof <luxtheluxof@gmail.com>
2026-03-17 22:28:15 -0700

    dam.n

--- src/main/java/com/luxof/remilia/mixin/BookTextParserMixin.java
+++ src/main/java/com/luxof/remilia/mixin/BookTextParserMixin.java
@@ -18,16 +18,15 @@ import net.minecraft.util.Formatting;
 import net.minecraft.util.Pair;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import vazkii.patchouli.client.book.text.BookTextParser;
-import vazkii.patchouli.client.book.text.BookTextParser.CommandProcessor;
 import vazkii.patchouli.client.book.text.Span;
 import vazkii.patchouli.client.book.text.SpanState;
 
@@ -266,9 +265,9 @@ public abstract class BookTextParserMixin {
 
     @Inject(
         method = "<clinit>()V",
-        at = @At("HEAD")
+        at = @At("TAIL")
     )
-    private static void remilia$staticConstructor() {
+    private static void remilia$staticConstructor(CallbackInfo ci) {
         BookTextParser.register(
             state -> {
                 state.modifyStyle(s -> s.withFormatting(Formatting.RESET));
